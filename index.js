2b20cd07d527af1ac0c913a8e6cb91005bd4e12f
vmheikki <veli-matti.heikkila@helsinki.fi>
2022-10-12 13:27:54 +0300

    added pg and pg-pool libraries to connect to postgres database
    added database.js service to use pg-pool connection

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const compression = require('compression');
+const database = require("./service/database");
 
 const ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 const port = process.env.OPENSHIFT_NODEJS_PORT || 3002;
@@ -17,6 +18,10 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+});
+
+database.query('SELECT NOW()', (err, res) => {
+    console.log(err ? "errors: " + err : 'Postgres client connected ' , res.rows[0]);
 });
 
 app.listen(port, ipaddress, () => {
