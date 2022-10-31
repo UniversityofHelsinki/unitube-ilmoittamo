5e3b07709d07428f56325b4f38ee58fd5a3bab39
sinkkila <reetta.sinkkila@helsinki.fi>
2022-10-31 08:45:17 +0200

    log mail path

--- service/emailService.js
+++ service/emailService.js
@@ -2,7 +2,8 @@ const axios = require('axios'); // https://www.npmjs.com/package/axios
 require("dotenv").config();
 
 exports.sendMail = async (to, series, video, archivedate) => {
-    const mailPath = process.env.ILMOITTAMO_EMAIL_SENDER_HOST_DEV + '/api/send';
+    const mailPath = process.env.ILMOITTAMO_EMAIL_SENDER_HOST  + '/api/send';
+    console.log(mailPath);
     const message = 'Hyvä ' + to + ', videosi '+ video + ' sarjasta ' + series + ' vanhentuu ' + archivedate + '. Mene https://lataamo.helsinki.fi ja tee jotain.';
     const response = await axios.post(mailPath,  {
         to: to,
