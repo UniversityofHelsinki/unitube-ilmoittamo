b3a6c61aa52e3ace869a9b0b00d0369be60dec49
sinkkila <reetta.sinkkila@helsinki.fi>
2022-11-02 08:22:02 +0200

    lataamo-718 update notification_sent_at

--- service/cron.js
+++ service/cron.js
@@ -1,5 +1,6 @@
 const cron = require("node-cron");
 const notify = require('./notify');
+const databaseService = require('./databaseService');
 
 cronJob = cron.schedule(process.env.CRON_START_TIME, async() => {
     console.log('Run CronJob job daily at ', process.env.CRON_START_TIME);
@@ -9,6 +10,7 @@ cronJob = cron.schedule(process.env.CRON_START_TIME, async() => {
         if (videoData.status == 200) {
             const seriesData = await notify.getSeriesData(videoData.data.is_part_of);
             await notify.createEmails(seriesData, video.archived_date, videoData);
+            await databaseService.updateNotificationSentAt(video.video_id);
         }
     }
 });
---
+++ service/databaseService.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+const database = require("./database");
+const Constants = require('../utils/constants');
+
+exports.updateNotificationSentAt = async(videoId) => {
+    const now = new Date();
+    const updateNotificationSentSQL = fs.readFileSync(path.resolve(__dirname, "../sql/updateNotificationSent.sql"), "utf8");
+    const updatedVideoEntry = await database.query(updateNotificationSentSQL, [now, videoId]);
+    return updatedVideoEntry.rowCount;
+};
+
+
