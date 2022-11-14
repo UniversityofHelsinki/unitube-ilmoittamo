d6a4c48284e355ccbe0faeaf0979e9e7569ff2a5
Antti Lankinen <antti.lankinen@helsinki.fi>
2022-11-14 10:38:26 +0200

    Tietokantaan kaksi aikaleimaa lisää

--- test/notifiedVideos.test.js
+++ test/notifiedVideos.test.js
@@ -24,7 +24,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
-    await client.query('CREATE TEMPORARY TABLE videos(video_id VARCHAR(255) NOT NULL, archived_date date, actual_archived_date date, deletion_date date, informed_date date, video_creation_date date, error_date date, notification_sent_at timestamp, skip_email boolean default false, PRIMARY KEY(video_id))');
+    await client.query('CREATE TEMPORARY TABLE videos(video_id VARCHAR(255) NOT NULL, archived_date date, actual_archived_date date, deletion_date date, video_creation_date date, error_date date, first_notification_sent_at timestamp, second_notification_sent_at timestamp, third_notification_sent_at timestamp, skip_email boolean default false, PRIMARY KEY(video_id))');
     await client.query('INSERT INTO videos (video_id, archived_date, video_creation_date) VALUES (\'e8a86433-0245-44b8-b0d7-69f6578bac6f\', \'2024-01-01\'::date, \'2008-01-01\'::date)');
     await client.query('INSERT INTO videos (video_id, archived_date, video_creation_date) VALUES (\'a637b33c-56a1-11ed-9b6a-0242ac120001\', \'2023-12-01\'::date, \'2009-01-01\'::date)');
     await client.query('INSERT INTO videos (video_id, archived_date, video_creation_date) VALUES (\'a637b65c-56a1-11ed-9b6a-0242ac120002\', \'2023-01-21\'::date, \'2010-01-01\'::date)');
@@ -64,7 +64,7 @@ describe('Video tests', () => {
         expect(videos.rows[1].video_id).toEqual('a637b8dc-56a1-11ed-9b6a-0242ac120004');
     });
 
-    it('should have notification_sent_at set after notification is sent', async () => {
+    it('should have first_notification_sent_at set after notification is sent', async () => {
         const videos = await notify.getVideosToSendNotification();
 
     });
