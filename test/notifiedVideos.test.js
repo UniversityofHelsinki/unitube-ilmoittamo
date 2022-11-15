46b0c54979ccff31d22b85dc621dc15ad450e90b
vmheikki <veli-matti.heikkila@helsinki.fi>
2022-11-15 14:27:26 +0200

    fixed tests

--- test/notifiedVideos.test.js
+++ test/notifiedVideos.test.js
@@ -27,12 +27,12 @@ beforeAll(async () => {
 
 beforeEach(async () => {
     await client.query('CREATE TEMPORARY TABLE videos(video_id VARCHAR(255) NOT NULL, archived_date date, actual_archived_date date, deletion_date date, informed_date date, video_creation_date date, error_date date, notification_sent_at timestamp, skip_email boolean default false, PRIMARY KEY(video_id))');
-    await client.query('INSERT INTO videos (video_id, archived_date, video_creation_date) VALUES (\'e8a86433-0245-44b8-b0d7-69f6578bac6f\', \'2022-12-01\'::date, \'2008-01-01\'::date)');
-    await client.query('INSERT INTO videos (video_id, archived_date, video_creation_date) VALUES (\'a637b33c-56a1-11ed-9b6a-0242ac120001\', \'2023-02-01\'::date, \'2009-01-01\'::date)');
+    await client.query('INSERT INTO videos (video_id, archived_date, video_creation_date) VALUES (\'e8a86433-0245-44b8-b0d7-69f6578bac6f\', current_date + interval \'3 months\' - INTERVAL \'30 DAY\', \'2008-01-01\'::date)');
+    await client.query('INSERT INTO videos (video_id, archived_date, video_creation_date) VALUES (\'a637b33c-56a1-11ed-9b6a-0242ac120001\', current_date + interval \'3 months\' - INTERVAL \'14 DAY\', \'2009-01-01\'::date)');
     await client.query('INSERT INTO videos (video_id, archived_date, video_creation_date) VALUES (\'a637b65c-56a1-11ed-9b6a-0242ac120002\', current_date + interval \'3 months\' - INTERVAL \'7 DAY\', \'2010-01-01\'::date)');
     await client.query('INSERT INTO videos (video_id, archived_date, video_creation_date) VALUES (\'a637b7ba-56a1-11ed-9b6a-0242ac120003\', current_date + interval \'3 months\' + INTERVAL \'7 DAY\', \'2011-01-01\'::date)');
-    await client.query('INSERT INTO videos (video_id, archived_date, video_creation_date) VALUES (\'a637b8dc-56a1-11ed-9b6a-0242ac120004\', \'2023-01-28\'::date, \'2012-01-01\'::date)');
-    await client.query('INSERT INTO videos (video_id, archived_date, video_creation_date, skip_email) VALUES (\'a637b8dc-56a1-11ed-9b6a-0242ac120005\', \'2023-01-28\'::date, \'2012-01-01\'::date, true)');
+    await client.query('INSERT INTO videos (video_id, archived_date, video_creation_date) VALUES (\'a637b8dc-56a1-11ed-9b6a-0242ac120004\', current_date + interval \'3 months\' + INTERVAL \'14 DAY\', \'2012-01-01\'::date)');
+    await client.query('INSERT INTO videos (video_id, archived_date, video_creation_date, skip_email) VALUES (\'a637b8dc-56a1-11ed-9b6a-0242ac120005\', current_date + interval \'3 months\' + INTERVAL \'30 DAY\', \'2012-01-01\'::date, true)');
 });
 
 afterEach(async () => {
