0dc8886ad5bbf1ea75bb9a5d17117b3ae470695c
Antti Lankinen <antti.lankinen@helsinki.fi>
2022-11-22 10:52:21 +0200

    export populateRecipientsMap

--- service/notify.js
+++ service/notify.js
@@ -142,6 +142,5 @@ module.exports = {
     getVideoData : getVideoData,
     createEmails: createEmails,
     getRecipientsMap: getRecipientsMap,
-    getNotifiedDate: getNotifiedDate,
     populateRecipientsMap: populateRecipientsMap
 };
