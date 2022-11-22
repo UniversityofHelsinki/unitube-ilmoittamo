bb9a4280d0c736340ad9f9eed86a3345e74a283e
vmheikki <veli-matti.heikkila@helsinki.fi>
2022-11-22 17:30:30 +0200

    tests for getRecipients function

--- test/getRecipientsMap.test.js
+++ test/getRecipientsMap.test.js
@@ -8,21 +8,21 @@ afterAll(async () => {
 
 jest.mock('../service/apiService');
 
-const videosToSendNotification = {
-    "command": "SELECT",
-    "rowCount": 1,
-    "oid": null,
-    "rows": [
-        {
-            "video_id": "cda5d8bf-eb6d-41a6-b7ae-6271f8ab0b99",
-            "archived_date": "15.02.2023"
-        },
-    ]
-};
 
+describe('Recipients tests', () => {
+    it('getRecipientsMap returns only unique recipients with one video with same contributor assigned directly and by group', async () => {
 
-describe('video recipients tests', () => {
-    it('getRecipientsMap returns only unique recipients even if recipient is returned from iam-group', async () => {
+        const videosToSendNotification = {
+            "command": "SELECT",
+            "rowCount": 1,
+            "oid": null,
+            "rows": [
+                {
+                    "video_id": "cda5d8bf-eb6d-41a6-b7ae-6271f8ab0b99",
+                    "archived_date": "15.02.2023"
+                },
+            ]
+        };
 
         apiService.getSeries.mockResolvedValue({
             status: 200,
@@ -60,6 +60,71 @@ describe('video recipients tests', () => {
 
         const recipientsInMap = await notify.getRecipientsMap(videosToSendNotification);
         expect(recipientsInMap.size).toEqual(7);
+        expect(recipientsInMap.has("mansikka@ad.helsinki.fi")).toBe(true);
         expect(recipientsInMap.get("mansikka@ad.helsinki.fi").length).toEqual(1);
+        expect(recipientsInMap.get('mansikka@ad.helsinki.fi')[0].video.identifier).toEqual('cda5d8bf-eb6d-41a6-b7ae-6271f8ab0b99');
+        expect(recipientsInMap.get('mansikka@ad.helsinki.fi')[0].video.title).toEqual('joku video');
+    });
+
+
+    it('getRecipientsMap returns only unique recipients with two videos with same contributor assigned directly and by group', async () => {
+
+        const videosToSendNotification = {
+            "command": "SELECT",
+            "rowCount": 2,
+            "oid": null,
+            "rows": [
+                {
+                    "video_id": "cda5d8bf-eb6d-41a6-b7ae-6271f8ab0b99",
+                    "archived_date": "15.02.2023"
+                },
+                {
+                    "video_id": "cda5d8bf-eb6d-41a6-b7ae-6271f8ab0b98",
+                    "archived_date": "15.02.2022"
+                }
+            ]
+        };
+
+
+        apiService.getSeries.mockResolvedValue({
+            status: 200,
+            data: {
+                identifier: '379fb94c-f194-422a-be6e-fc24f9507b95',
+                title: 'joku sarja',
+                contributors: ['grp-mansikanpoimijat', 'mansikka']
+            }
+        });
+
+        apiService.getEvent.mockResolvedValue({
+            status: 200,
+            data: {
+                identifier: 'cda5d8bf-eb6d-41a6-b7ae-6271f8ab0b99',
+                title: 'joku video',
+            }
+        });
+
+        apiService.getRecipients.mockResolvedValue({
+            status: 200,
+            data: {
+                "group": "grp-mansikanpoimijat",
+                "members": [
+                    "mustikka",
+                    "mansikka",
+                    "puolukka",
+                    "karpalo",
+                    "mesimarja",
+                    "vadelma",
+                    "mustaherukka"
+                ]
+            }
+        });
+
+
+        const recipientsInMap = await notify.getRecipientsMap(videosToSendNotification);
+        expect(recipientsInMap.size).toEqual(7);
+        expect(recipientsInMap.has("mansikka@ad.helsinki.fi")).toBe(true);
+        expect(recipientsInMap.get('mansikka@ad.helsinki.fi')[0].video.identifier).toEqual('cda5d8bf-eb6d-41a6-b7ae-6271f8ab0b99');
+        expect(recipientsInMap.get('mansikka@ad.helsinki.fi')[0].video.title).toEqual('joku video');
+        expect(recipientsInMap.get("mansikka@ad.helsinki.fi").length).toEqual(2);
     });
 });
