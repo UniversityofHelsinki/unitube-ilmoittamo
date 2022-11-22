1b26dd793534deec4686474e12023cbc119722ef
vmheikki <veli-matti.heikkila@helsinki.fi>
2022-11-22 17:44:42 +0200

    tests for getRecipients function

--- test/getRecipientsMap.test.js
+++ test/getRecipientsMap.test.js
@@ -127,4 +127,79 @@ describe('Recipients tests', () => {
         expect(recipientsInMap.get('mansikka@ad.helsinki.fi')[0].video.title).toEqual('joku video');
         expect(recipientsInMap.get("mansikka@ad.helsinki.fi").length).toEqual(2);
     });
+
+    it('getRecipientsMap returns only unique recipients and one video if recipient is not found in group', async () => {
+
+        const videosToSendNotification = {
+            "command": "SELECT",
+            "rowCount": 2,
+            "oid": null,
+            "rows": [
+                {
+                    "video_id": "cda5d8bf-eb6d-41a6-b7ae-6271f8ab0b99",
+                    "archived_date": "15.02.2023"
+                },
+                {
+                    "video_id": "cda5d8bf-eb6d-41a6-b7ae-6271f8ab0b98",
+                    "archived_date": "15.02.2022"
+                }
+            ]
+        };
+
+
+        apiService.getSeries.mockResolvedValueOnce({
+            status: 200,
+            data: {
+                identifier: '379fb94c-f194-422a-be6e-fc24f9507b95',
+                title: 'joku sarja',
+                contributors: ['grp-mansikanpoimijat', 'mansikka']
+            }
+        });
+
+        apiService.getSeries.mockResolvedValueOnce({
+            status: 200,
+            data: {
+                identifier: '379fb94c-f194-422a-be6e-fc24f9507b98',
+                title: 'joku sarja',
+                contributors: ['grp-mansikanpoimijat', 'mansikka', 'variksenmarja']
+            }
+        });
+
+        apiService.getEvent.mockResolvedValue({
+            status: 200,
+            data: {
+                identifier: 'cda5d8bf-eb6d-41a6-b7ae-6271f8ab0b99',
+                title: 'joku video',
+            }
+        });
+
+        apiService.getRecipients.mockResolvedValue({
+            status: 200,
+            data: {
+                "group": "grp-mansikanpoimijat",
+                "members": [
+                    "mustikka",
+                    "mansikka",
+                    "puolukka",
+                    "karpalo",
+                    "mesimarja",
+                    "vadelma",
+                    "mustaherukka"
+                ]
+            }
+        });
+
+
+        const recipientsInMap = await notify.getRecipientsMap(videosToSendNotification);
+        expect(recipientsInMap.size).toEqual(8);
+        expect(recipientsInMap.has("mansikka@ad.helsinki.fi")).toBe(true);
+        expect(recipientsInMap.get('mansikka@ad.helsinki.fi')[0].video.identifier).toEqual('cda5d8bf-eb6d-41a6-b7ae-6271f8ab0b99');
+        expect(recipientsInMap.get('mansikka@ad.helsinki.fi')[0].video.title).toEqual('joku video');
+        expect(recipientsInMap.get("mansikka@ad.helsinki.fi").length).toEqual(2);
+        expect(recipientsInMap.has("variksenmarja@ad.helsinki.fi")).toBe(true);
+        expect(recipientsInMap.get('variksenmarja@ad.helsinki.fi')[0].video.identifier).toEqual('cda5d8bf-eb6d-41a6-b7ae-6271f8ab0b99');
+        expect(recipientsInMap.get('variksenmarja@ad.helsinki.fi')[0].video.title).toEqual('joku video');
+        expect(recipientsInMap.get("variksenmarja@ad.helsinki.fi").length).toEqual(1);
+    });
+
 });
