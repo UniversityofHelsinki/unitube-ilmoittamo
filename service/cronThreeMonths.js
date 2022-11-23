2875ea430d5b395b9f3b50291dff43f057a27cf6
sinkkila <reetta.sinkkila@helsinki.fi>
2022-11-23 13:38:13 +0200

    lataamo-760 log to console instead of a file

--- service/cronThreeMonths.js
+++ service/cronThreeMonths.js
@@ -2,9 +2,10 @@ const cronThreeMonths = require("node-cron");
 const notify = require('./notify');
 const databaseService = require('./databaseService');
 const constants = require("../utils/constants");
+const logger = require("../utils/winstonLogger");
 
 cronJobThreeMonths = cronThreeMonths.schedule(process.env.CRON_START_TIME_THIRTEEN_DAYS, async() => {
-    console.log('Run cronJobThreeMonths job at ', process.env.CRON_START_TIME_THIRTEEN_DAYS);
+    logger.info('Run cronJobThreeMonths job at ', process.env.CRON_START_TIME_THIRTEEN_DAYS);
     const videosToSendNotification = await notify.getVideosToSendNotification(constants.VIDEO_NOTIFIED_THREE_MONTHS, constants.VIDEO_NOTIFIED_INTERVAL_ONE_WEEK, null, null, null);
     const recipientsMap = await notify.getRecipientsMap(videosToSendNotification);
     await notify.createEmails(recipientsMap);
--- utils/winstonLogger.js
+++ utils/winstonLogger.js
@@ -1,8 +1,5 @@
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf } = format;
-const DailyRotateFile = require('winston-daily-rotate-file');
-
-const LOG_DIR = './logs';
 
 
 const lataamoFormat = printf(({ level, message, label, timestamp }) => {
@@ -11,26 +8,14 @@ const lataamoFormat = printf(({ level, message, label, timestamp }) => {
 
 
 const logger = createLogger({
+    transports: [
+        new transports.Console()
+    ],
     format: combine(
         label({ label: 'Lataamo ilmoittamo' }),
-        timestamp(),
+        timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
         lataamoFormat
     ),
-    transports: [
-        // https://medium.com/@davidmcintosh/winston-a-better-way-to-log-793ac19044c5
-        new DailyRotateFile({
-            filename: `${LOG_DIR}/lataamo-info-%DATE%.log`,
-            level: 'info',
-            datePattern: 'YYYY-MM-DD'}),
-        new DailyRotateFile({
-            filename: `${LOG_DIR}/lataamo-warning-%DATE%.log`,
-            level: 'warn',
-            datePattern: 'YYYY-MM-DD'}),
-        new DailyRotateFile({
-            filename: `${LOG_DIR}/lataamo-error-%DATE%.log`,
-            level: 'error',
-            datePattern: 'YYYY-MM-DD'})
-    ]
 });
 
 module.exports = logger;
