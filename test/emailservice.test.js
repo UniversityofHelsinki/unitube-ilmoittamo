f31842b2cb53dd57c429a6dcb1082d7c9fd9e972
Antti Lankinen <antti.lankinen@helsinki.fi>
2022-12-01 10:19:10 +0200

    testi korjattu.

--- test/emailservice.test.js
+++ test/emailservice.test.js
@@ -1,7 +1,30 @@
 const email = require("../service/emailService");
 const axios = require('axios');
+const Pool = require("pg-pool");
+const client = require("../service/database");
 
 jest.mock('axios');
+
+beforeAll(async () => {
+    const pool = new Pool({
+        user: process.env.POSTGRES_USER,
+        host: process.env.HOST,
+        database: process.env.DATABASE,
+        password: process.env.PASSWORD,
+        port: process.env.PORT,
+        ssl: process.env.SSL ? true : false,
+        max: 1, // Reuse the connection to make sure we always hit the same temporal schema
+        idleTimeoutMillis: 0 // Disable auto-disconnection of idle clients to make sure we always hit the same temporal schema
+    });
+
+    client.end = () => {
+        return pool.end();
+    };
+
+    client.query = (text, values) => {
+        return pool.query(text, values);
+    };
+});
 
 test('sending mail returns create status', async () => {
     axios.post.mockResolvedValue({
@@ -14,3 +37,8 @@ test('sending mail returns create status', async () => {
     const mail = await email.sendMail('joku@jossain.com', [{video : {identifier : "12312312312", title: "testivideo", archivedDate: "01.01.2021" }, series : {title : "koesarja"}}]);
     expect(mail.status).toBe('create');
 })
+
+afterAll( done => {
+    client.end().then(done());
+});
+
