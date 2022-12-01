744f4fe5efbe3bb9445487fb164456aec42c2c7c
Emmi Sulander <emmi.sulander@helsinki.fi>
2022-12-01 12:18:33 +0200

    Move temp table creation and dropping inside test

--- test/emailservice.test.js
+++ test/emailservice.test.js
@@ -26,16 +26,9 @@ beforeAll(async () => {
     };
 });
 
-beforeEach(async () => {
-    await client.query('CREATE TEMPORARY TABLE email_templates(id SERIAL NOT NULL, name VARCHAR(255) UNIQUE NOT NULL, description VARCHAR(255), subject VARCHAR(255), header_fi TEXT, footer_fi TEXT, header_sv TEXT, footer_sv TEXT, header_en TEXT, footer_en TEXT, modified TIMESTAMP, PRIMARY KEY(id))');
-});
-
-afterEach('Drop temporary tables', async () => {
-    await wait(10);
-    await client.query('DROP TABLE pg_temp.email_templates');
-});
-
 test('sending mail returns create status', async () => {
+    await client.query('CREATE TEMPORARY TABLE email_templates(id SERIAL NOT NULL, name VARCHAR(255) UNIQUE NOT NULL, description VARCHAR(255), subject VARCHAR(255), header_fi TEXT, footer_fi TEXT, header_sv TEXT, footer_sv TEXT, header_en TEXT, footer_en TEXT, modified TIMESTAMP, PRIMARY KEY(id))');
+    
     axios.post.mockResolvedValue({
         data: {
             status: 'create',
@@ -45,6 +38,9 @@ test('sending mail returns create status', async () => {
 
     const mail = await email.sendMail('joku@jossain.com', [{video : {identifier : "12312312312", title: "testivideo", archivedDate: "01.01.2021" }, series : {title : "koesarja"}}]);
     expect(mail.status).toBe('create');
+
+    await wait(10);
+    await client.query('DROP TABLE pg_temp.email_templates');
 })
 
 afterAll( done => {
