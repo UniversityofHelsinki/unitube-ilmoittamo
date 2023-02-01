a880bdd00f1fb47eee0fe7496f2a2b5ff1c4d93d
vmheikki <veli-matti.heikkila@helsinki.fi>
2023-02-01 14:54:09 +0200

    ability to run jobs from api

--- service/cronOneWeek.js
+++ service/cronOneWeek.js
@@ -3,13 +3,19 @@ const notify = require("./notify");
 const constants = require("../utils/constants");
 const databaseService = require("./databaseService");
 const logger = require("../utils/winstonLogger");
+const {runOneMonthJob} = require("./cronOneMonth");
 
-cronJobOneWeek = cron.schedule(process.env.CRON_START_TIME_THREE_DAYS, async() => {
-    logger.info(`Run cronJobOneWeek job at ${process.env.CRON_START_TIME_THREE_DAYS}`);
+const runOneWeekJob =  async() => {
     const videosToSendNotification = await notify.getVideosToSendNotification(null, null, null, constants.VIDEO_NOTIFIED_START_SIX_DAYS, constants.VIDEO_NOTIFIED_END_NINE_DAYS);
     const recipientsMap = await notify.getRecipientsMap(videosToSendNotification);
     await notify.createEmails(recipientsMap);
     await databaseService.updateNotificationSentAt(videosToSendNotification, constants.ONE_WEEK);
+}
+
+cronJobOneWeek = cron.schedule(process.env.CRON_START_TIME_THREE_DAYS, async() => {
+    logger.info(`Run cronJobOneWeek job at ${process.env.CRON_START_TIME_THREE_DAYS}`);
+    await runOneWeekJob();
 });
 
 module.exports.cronJobOneWeek = cronJobOneWeek;
+module.exports.runOneWeekJob = runOneMonthJob;
